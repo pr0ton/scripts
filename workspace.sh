29851a1c70b7e82d85c622b2308c85e44d9625d5
pr0ton <pratiktandel99@gmail.com>
2017-03-19 18:45:58 -0700

    Delete workspace.sh

--- workspace.sh
+++
@@ -1,7 +0,0 @@
-cd ~
-mkdir workspace
-cd workspace
-
-# Private repos, but ok to list here
-git clone git@bitbucket.org:pr0ton/pzbot-sparkle.git
-git clone git@bitbucket.org:pr0ton/personal-website.git 
